34bf79f7d32048fe7905c527850b9813654ec25a
abelcg <abelcg86@gmail.com>
2021-12-07 10:57:41 -0300

    se instalo nodemon

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import express from 'express';
 
+const app = express();
 
+const port = 4000;
 
+app.listen(port, ()=> console.log('Estoy en el puerto ' + port));
 
 console.log('hola mundo');
+
+
