314fb7028ea78099133bfe45084e6647d95d4b8b
abelcg <abelcg86@gmail.com>
2021-12-14 11:37:48 -0300

    se agrego la conexion a la base de datos

---
+++ src/database.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose'
+
+const url = 'mongodb://localhost:27017/crudcafe';
+
+mongoose.connect(url,{useNewUrlParser: true});
+
+//guardar la conexion
+const connection = mongoose.connection;
+
+connection.once('open', () => {
+    console.log('BD conectada')
+});
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import morgan from 'morgan';
 import cors from 'cors';
 import path from 'path';
 import router from './routes/producto.routes';
+import './database';
 
 
 // instanciar express
@@ -17,10 +18,10 @@ app.listen(app.get('port'), ()=> {
 });
 
 //configuraciones extras midlewares
-app.use(morgan('dev'));
-app.use(cors());
+app.use(morgan('dev')); // nos da info en la consola del status de la peticion
+app.use(cors()); //nos permite recibir peticiones remotas al servidor
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true })); //estos dos ultimos permiten recibir e interpretar el json de la resp
 
 // configurar el index.html
 app.use(express.static(path.join(__dirname, '../public')))
