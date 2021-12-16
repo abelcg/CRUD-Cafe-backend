6aa49e06b2683bd97dc92fe237cc1f18637ef0b3
abelcg <abelcg86@gmail.com>
2021-12-16 09:59:28 -0300

    se creo la logica de crearProducto

--- src/controllers/producto.controllers.js
+++ src/controllers/producto.controllers.js
@@ -6,7 +6,24 @@ productoCtrl.borrarProducto = (req, res)=> {
     res.send('alguien quiere borrar producto')
   };
 
-productoCtrl.nuevoProducto = (req, res)=> {
+productoCtrl.nuevoProducto = async(req, res)=> {
+  try {
+    console.log(req.body);
+    //validar
+    //crear un objeto para guardar en la BD
+    const productoNuevo = new Producto({
+      nombreProducto: req.body.nombreProducto,
+      precioProducto: req.body.precioProducto,
+      categoria: req.body.categoria
+    })
+    //console.log(productoNuevo);
+    //guardar BD
+    await productoNuevo.save();
+    res.status(201).json({mensaje: 'Producto creado correctamente'});
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({mensaje: 'Error al crear producto'});
+  }
     res.send('alguien quiere agregar producto')
 };  
 
